021067b7f65d463f5263f306d8e6ab7e8db26598
Stephen Adams <33077783+StephenAdams-hamont@users.noreply.github.com>
2024-10-19 15:06:44 -0400

    Updated player1/player2 files with a get function to prevent requiring list references to be used in order to build move logic based on the current board.

--- Tic-Tac-Toe-Example/Player1.py
+++ Tic-Tac-Toe-Example/Player1.py
@@ -2,7 +2,16 @@
 #
 # author: Stephen Adams
 # date  : 2024-10-19
+#
+# This file contains the code for Player 1 in a Tic-Tac-Toe game. It is intentionally not simplified or generalized to
+# attempt to avoid merge conflicts between the partners.
 import random
+
+def getValue( board, position ):
+    """
+    Returns the value at the given position on the board, from the simple 1-9 index.
+    """
+    return board[ (position - 1) // 3 ][ (position - 1) % 3 ]
 
 def getMove(board):
     """
@@ -16,6 +25,7 @@ def getMove(board):
         ---------
         7 | 8 | 9
 
+    To avoid requring list notation in this function, we will use the getValue function to get the value at a given position.
 
     Parameters:
     board (list of list of str): The current state of the Tic-Tac-Toe board.
@@ -24,4 +34,8 @@ def getMove(board):
     integer: The index of the next move between 1 and 9.
     """
     
+    # Example of checking for a win in the top row.
+    if ( getValue( board, 1 ) == "X" and getValue( board, 2 ) == "X" and getValue( board, 3 ) == " " ):
+        return 3
+
     return random.randint(1, 9) # This is a placeholder. Replace this line with your code.
--- Tic-Tac-Toe-Example/Player2.py
+++ Tic-Tac-Toe-Example/Player2.py
@@ -2,11 +2,20 @@
 #
 # author: Stephen Adams
 # date  : 2024-10-19
+#
+# This file contains the code for Player 2 in a Tic-Tac-Toe game. It is intentionally not simplified or generalized to
+# attempt to avoid merge conflicts between the partners
 import random
+
+def getValue( board, position ):
+    """
+    Returns the value at the given position on the board, from the simple 1-9 index.
+    """
+    return board[ (position - 1) // 3 ][ (position - 1) % 3 ]
 
 def getMove(board):
     """
-    Determines the next move for Player 1. Player 1 will always be 'X'.
+    Determines the next move for Player 2. Player 2 will always be 'O'.
     
     Since studnets will not have yet learned lists, we will use indicies 1-9 to represent the Tic-Tac-Toe board as follows:
 
@@ -16,6 +25,7 @@ def getMove(board):
         ---------
         7 | 8 | 9
 
+    To avoid requring list notation in this function, we will use the getValue function to get the value at a given position.
 
     Parameters:
     board (list of list of str): The current state of the Tic-Tac-Toe board.
@@ -24,4 +34,8 @@ def getMove(board):
     integer: The index of the next move between 1 and 9.
     """
     
+    # Example of checking for a win in the top row.
+    if ( getValue( board, 1 ) == "O" and getValue( board, 2 ) == "O" and getValue( board, 3 ) == " " ):
+        return 3
+
     return random.randint(1, 9) # This is a placeholder. Replace this line with your code.
